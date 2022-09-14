ae9ba93d9d16d00a75e27807e3e6b27948fec5e8
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-09-14 22:56:22 +0700

    add intro screen part 1

--- lib/representation/screen/intro_screen.dart
+++ lib/representation/screen/intro_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:travo_app/core/constants/dismension_constants.dart';
+import 'package:travo_app/core/helpers/asset_helper.dart';
+import 'package:travo_app/core/helpers/image_helper.dart';
+
+import '../../core/constants/textstyle_constants.dart';
 
 class IntroScreen extends StatefulWidget {
   const IntroScreen({Key? key}) : super(key: key);
@@ -9,10 +14,64 @@ class IntroScreen extends StatefulWidget {
 }
 
 class _IntroScreenState extends State<IntroScreen> {
+  final PageController _pageController = PageController();
+
+  Widget _buildItemIntroScreen(String image, String title, String description,
+      AlignmentGeometry alignment) {
+    return Column(
+      children: [
+        Container(
+            alignment: alignment,
+            child: ImageHelper.loadFromAsset(image,
+                height: 375, fit: BoxFit.fitHeight)),
+        const SizedBox(
+          height: kMediumPadding * 2,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kMediumPadding),
+          child: Column(
+            children: [
+              Text(
+                title,
+                style: TextStyles.defaultStyle.bold,
+              ),
+              const SizedBox(
+                height: kMediumPadding,
+              ),
+              Text(
+                description,
+                style: TextStyles.defaultStyle,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: PageView(
+        controller: _pageController,
+        children: [
+          _buildItemIntroScreen(
+              AssetHelper.slide1,
+              "Book a flight",
+              "Found a flight that matches your destination and schedule? Book it instantly.",
+              Alignment.centerRight),
+          _buildItemIntroScreen(
+              AssetHelper.slide2,
+              "Find a hotel room",
+              "Select the day, book your room. We give you the best price.",
+              Alignment.center),
+          _buildItemIntroScreen(
+              AssetHelper.slide3,
+              "Enjoy your trip",
+              "Easy discovering new places and share these between your friends and travel together.",
+              Alignment.centerLeft),
+        ],
+      ),
     );
   }
 }
