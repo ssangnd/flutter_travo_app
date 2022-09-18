3fdf19ad60e6649edb7188ada4d2a2173fc5e3b6
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-09-18 23:40:35 +0700

    add bottom tab

--- lib/representation/screen/main_app.dart
+++ lib/representation/screen/main_app.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:travo_app/core/constants/color_constants.dart';
+import 'package:travo_app/core/constants/dismension_constants.dart';
+import 'package:travo_app/representation/screen/home_screen.dart';
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -10,11 +15,67 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("MainApp"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          HomeScreen(),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.orange,
+          ),
+          Container(
+            color: Colors.brown,
+          ),
+        ],
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: ColorPalette.primaryColor,
+        unselectedItemColor: ColorPalette.primaryColor.withOpacity(0.2),
+        margin: const EdgeInsets.symmetric(
+            horizontal: kMediumPadding, vertical: kDefaultPadding),
+        items: [
+          SalomonBottomBarItem(
+            icon: const Icon(
+              FontAwesomeIcons.house,
+              size: kDefaultIconSize,
+            ),
+            title: const Text("Home"),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(
+              FontAwesomeIcons.solidHeart,
+              size: kDefaultIconSize,
+            ),
+            title: const Text("Likes"),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(
+              FontAwesomeIcons.briefcase,
+              size: kDefaultIconSize,
+            ),
+            title: const Text("Booking"),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(
+              FontAwesomeIcons.solidUser,
+              size: kDefaultIconSize,
+            ),
+            title: const Text("Profile"),
+          ),
+        ],
       ),
     );
   }
