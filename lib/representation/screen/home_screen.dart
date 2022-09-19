b0a01e0a0a9da89d9efba522cf7de9cfdd62e94f
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-09-19 21:52:35 +0700

    Create Custom AppBar - Part 3

--- lib/representation/screen/home_screen.dart
+++ lib/representation/screen/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travo_app/core/constants/color_constants.dart';
+import 'package:travo_app/core/constants/dismension_constants.dart';
+import 'package:travo_app/core/helpers/image_helper.dart';
 
+import '../../core/helpers/asset_helper.dart';
 import '../widgets/app_bar-container.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,12 +20,66 @@ class _HomeScreenState extends State<HomeScreen> {
     // return Container();
     return AppBarContainerWidget(
       // title: const Text("Home"),
-      titleString: 'Home',
-      implementLeading: true,
-      implementTraling: true,
-      child: Container(
-        color: Colors.amber,
+      // titleString: 'Home',
+      // implementLeading: true,
+      // implementTraling: true,
+      title: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kDefaultPadding,
+        ),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Hi Jane!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: kMediumPadding,
+                ),
+                Text(
+                  'Where are you going next?',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            const Icon(
+              FontAwesomeIcons.bell,
+              size: kDefaultIconSize,
+              color: Colors.white,
+            ),
+            const SizedBox(
+              width: kTopPadding,
+            ),
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  kItemPadding,
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.all(kMinPadding),
+              child: ImageHelper.loadFromAsset(
+                AssetHelper.person,
+              ),
+            ),
+          ],
+        ),
       ),
+      child: Container(
+          // color: Colors.amber,
+          ),
     );
   }
 }
