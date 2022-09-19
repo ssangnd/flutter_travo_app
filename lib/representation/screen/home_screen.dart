287a82cc32ceffd5bf978d4f171bd92ca8489e92
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-09-19 23:24:44 +0700

    Make HomeScreen App

--- lib/representation/screen/home_screen.dart
+++ lib/representation/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travo_app/core/constants/color_constants.dart';
 import 'package:travo_app/core/constants/dismension_constants.dart';
 import 'package:travo_app/core/helpers/image_helper.dart';
+import 'package:travo_app/representation/screen/hotel_screen.dart';
 
 import '../../core/helpers/asset_helper.dart';
 import '../widgets/app_bar-container.dart';
@@ -15,6 +16,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildItemCategory(
+      Widget icon, Color color, Function() onTap, String title) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(
+              vertical: kMediumPadding,
+            ),
+            decoration: BoxDecoration(
+              color: color.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(kItemPadding),
+            ),
+            child: icon,
+          ),
+          const SizedBox(
+            height: kItemPadding,
+          ),
+          Text(title),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // return Container();
@@ -77,9 +104,85 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-      child: Container(
-          // color: Colors.amber,
+      // child: Container(
+      // color: Colors.amber,
+      //     ),
+      child: Column(
+        children: [
+          const TextField(
+            decoration: InputDecoration(
+              hintText: 'Search your destination',
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(kTopPadding),
+                child: Icon(
+                  FontAwesomeIcons.magnifyingGlass,
+                  color: Colors.black,
+                  size: kDefaultIconSize,
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    kItemPadding,
+                  ),
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: kItemPadding,
+              ),
+            ),
           ),
+          const SizedBox(
+            height: kDefaultPadding,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(
+                      AssetHelper.icoHotel,
+                      width: kBottomBarIconSize,
+                      height: kBottomBarIconSize,
+                    ),
+                    const Color(0xffFE9C5E), () {
+                  Navigator.of(context).pushNamed(HotelScreen.routeName);
+                }, 'Hotels'),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(
+                      AssetHelper.icoPlane,
+                      width: kBottomBarIconSize,
+                      height: kBottomBarIconSize,
+                    ),
+                    const Color(0xddF77777),
+                    () => {},
+                    'Flight'),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(
+                      AssetHelper.icoHotelPlane,
+                      width: kBottomBarIconSize,
+                      height: kBottomBarIconSize,
+                    ),
+                    const Color(0xff3EC8BC),
+                    () => {},
+                    'All'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:travo_app/representation/screen/hotel_screen.dart';
 import 'package:travo_app/representation/screen/intro_screen.dart';
 import 'package:travo_app/representation/screen/main_app.dart';
 import 'package:travo_app/representation/screen/splash_creen.dart';
@@ -7,4 +8,5 @@ final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => const SplashScreen(),
   IntroScreen.routeName: (context) => const IntroScreen(),
   MainApp.routeName: (context) => const MainApp(),
+  HotelScreen.routeName: (context) => const HotelScreen(),
 };
